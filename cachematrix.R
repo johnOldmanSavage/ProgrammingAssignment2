1ecfd849280a17fabaf120e063a712f535cfd654
John Oldman <john.oldman.savage@gmail.com>
2016-02-13 19:08:55 +0100

    edited cachematrix.R for submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## two functions that cache the inverse of a matrix
 
-## Write a short comment describing this function
+## create a special matrix object that can cache its inverse
+## returns the created matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        x.inv <- NULL
+        set <- function(y){
+          x <<- y
+          x.inv <<- NULL
+        }
+        get <- function() x
+        setInv <- function(invMatrix) x.inv <<- invMatrix
+        getInv <- function() x.inv
+        list(set=set,get=get,setInv=setInv,getInv=getInv)
 }
 
 
-## Write a short comment describing this function
+## if not in the cache, this function computes
+## the inverse of the special matrix returned by the function above
+## the input matrix is assumed to be invertible
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) { 
+       ## Return a matrix that is the inverse of 'x'
+       x.inv <- x$getInv()
+       if(!is.null(x.inv)) {
+           return(x.inv)
+      }else{
+          mat <- x$get()
+          x.inv <- solve(mat) #this will work only with an invertible matrix, which the assumtion here
+          x$setInv(x.inv)
+          return(x.inv)
+      }
 }
+
+
+## a simple test
+## a <- cacheSolve(makeCacheMatrix(matrix(1:4,2,2)))
+## a %*% matrix(1:4,2,2)
+## [,1] [,2]
+## [1,]    1    0
+## [2,]    0    1
+       
+
